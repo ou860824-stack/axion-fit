835a3def81ad29d6d06320d07a6d0cce7a3203dd
ou860824-stack <ou860824@gmail.com>
2026-09-25 01:57:23 +0800

    Add GA4 tracking to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,17 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500;700;900&family=JetBrains+Mono:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZPGRK83KRS"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ZPGRK83KRS');
+            `,
+          }}
+        />
       </head>
       <body className="min-h-full flex flex-col bg-bg text-text">
         <Header />
